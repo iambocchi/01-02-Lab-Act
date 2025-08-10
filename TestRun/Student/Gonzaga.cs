84ac74fc9f070dd45cfd5d461eac26bcf4a6049c
yeye <purielgonzaga@gmail.com>
2025-08-10 21:51:56 +0800

    Added Laboratory Exercise 1 & 2 by Gonzaga
    straggled alot bcuz of how slow my laptop is ahhhhhhh
    
    *nahingi po ng sponsor

--- TestRun/Student/Gonzaga.cs
+++ TestRun/Student/Gonzaga.cs
@@ -52,7 +52,7 @@ namespace TestRun.Student
 
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
-
+// test update
             Console.WriteLine("Author by: "+ name);
         }
    
